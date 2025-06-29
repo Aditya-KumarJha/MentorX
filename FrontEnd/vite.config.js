9763891b6d89f16b22e59e61b0ddfce2c439556c
adityajha <aj12102003@gmail.com>
2025-06-29 19:43:52 +0530

    fix: ensure _redirects copied via vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()],
-})
+  plugins: [tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'), 
+      },
+    },
+  },
+  publicDir: 'public', 
+});
